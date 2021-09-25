8ec8fe7ff3e8c8b6e479d924a037f57c6a05c07c
gusya1 <serheos@bk.ru>
2021-09-25 21:36:14 +0300

    add documents as module

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+import setuptools
 
-setup(name='MSApi',
-      version='0.0.1',
-      description='Python API for manage MoySklad',
-      packages=['MSApi'],
-      author_email='serheos@gmail.com',
-      zip_safe=False)
+setuptools.setup(name='MSApi',
+                 version='0.0.6',
+                 description='Python API for manage MoySklad',
+                 packages=setuptools.find_packages(),
+                 install_requires=["requests>=2.22.0"],
+                 author_email='serheos@gmail.com',
+                 zip_safe=False)
